60db4eb17e1cd64ce1350f11d9ffa9957aabb0d4
Luciano Souza <souzaluciano9517@gmail.com>
2022-02-10 01:11:10 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { TransactionsModule } from './transactions/transactions.module';
+
+@Module({
+  imports: [TransactionsModule,
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database:'db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize:true
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
